f57b609676820bf28974a0092649ecd0f75874db
Guillaume Knispel <gknispel@proformatique.com>
2008-01-15 15:29:48 +0000

    [license] updated copyright years
    git-svn-id: svn+ssh://rcs.lan-puteaux.avencall.com/var/lib/svn/xivo/trunk/cticlients@2324 dc85baef-7018-0410-bfbd-8a639da648bb

--- common/xivoconsts.h
+++ common/xivoconsts.h
@@ -1,5 +1,5 @@
 /* XIVO CTI clients
-Copyright (C) 2007  Proformatique
+Copyright (C) 2007, 2008  Proformatique
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
