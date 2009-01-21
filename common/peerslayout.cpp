17c7d68e4024b4050c9a6002951a2d396fc68b8c
Marc Dequènes (Duck) <mdequenes@proformatique.com>
2009-01-21 18:30:09 +0000

    [RM] updated copyright years
    git-svn-id: svn+ssh://rcs.lan-puteaux.avencall.com/var/lib/svn/xivo/trunk/cticlients@5171 dc85baef-7018-0410-bfbd-8a639da648bb

--- common/peerslayout.cpp
+++ common/peerslayout.cpp
@@ -1,5 +1,5 @@
 /* XIVO CTI clients
- * Copyright (C) 2007, 2008  Proformatique
+ * Copyright (C) 2007-2009  Proformatique
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
