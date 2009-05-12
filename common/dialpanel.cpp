69a93781072b0717cfcf9328ce30e543d68cdba8
Marc Dequènes (Duck) <mdequenes@proformatique.com>
2009-05-12 14:05:54 +0000

    [evol] revised licensing terms to allow linking with OpenSSL (yet limited to an example in 'common/dialpanel.cpp')
    git-svn-id: svn+ssh://rcs.lan-puteaux.avencall.com/var/lib/svn/xivo/trunk/cticlients@6215 dc85baef-7018-0410-bfbd-8a639da648bb

--- common/dialpanel.cpp
+++ common/dialpanel.cpp
@@ -1,3 +1,23 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version, with a Section 7 Additional
+# Permission as follows:
+#   This notice constitutes a grant of such permission as is necessary
+#   to combine or link this software, or a modified version of it, with
+#   the MySQL Client Library, as published by Sun Microsystems and/or
+#   MySQL AB, or a derivative work of the MySQL CLient LIbrary, and to
+#   copy, modify, and distribute the resulting work. The MySQL Client
+#   LIbrary is licensed under version 2 of the GNU General Public
+#   License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* XiVO Client
  * Copyright (C) 2007-2009, Proformatique
  *
@@ -6,7 +26,17 @@
  * XiVO Client is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * (at your option) any later version, with a Section 7 Additional
+ * Permission as follows:
+ *   This notice constitutes a grant of such permission as is necessary
+ *   to combine or link this software, or a modified version of it, with
+ *   the OpenSSL project's "OpenSSL" library, or a derivative work of it,
+ *   and to copy, modify, and distribute the resulting work. This is an
+ *   extension of the special permission given by Trolltech to link the
+ *   Qt code with the OpenSSL library (see
+ *   <http://doc.trolltech.com/4.4/gpl.html>). The OpenSSL library is
+ *   licensed under a dual license: the OpenSSL License and the original
+ *   SSLeay license.
  *
  * XiVO Client is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,13 +45,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with XiVO Client.  If not, see <http://www.gnu.org/licenses/>.
- *
- * In addition, as a special exception, Trolltech gives permission to link
- * the code of its release of Qt with the OpenSSL project's "OpenSSL" library
- * (or modified versions of the "OpenSSL" library that use the same license
- * as the original version), and distribute the linked executables.
- *
- * (see <http://doc.trolltech.com/4.4/gpl.html>)
  */
 
 /* $Revision$
