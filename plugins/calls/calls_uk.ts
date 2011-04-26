5cb8f2d9e1e5d9721bd8162268d1ea1a4f385fb5
Corentin Le Gall <clegall@proformatique.com>
2011-04-26 15:59:11 +0200

    update uk files again

--- plugins/calls/calls_uk.ts
+++ plugins/calls/calls_uk.ts
@@ -1,60 +1,62 @@
-<?xml version="1.0" encoding="utf8"?><!DOCTYPE TS><TS language="uk_UA" version="2.0">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk_UA">
 <context>
     <name>CallWidget</name>
     <message>
         <location filename="src/callwidget.cpp" line="89"/>
         <source>&amp;Hangup</source>
-        <translation type="unfinished">&amp;Повісити слухавку</translation>
+        <translation>&amp;Повісити слухавку</translation>
     </message>
     <message>
         <location filename="src/callwidget.cpp" line="90"/>
         <source>Hang up/Close the channel</source>
-        <translation type="unfinished">Закрити канал</translation>
+        <translation>Закрити канал</translation>
     </message>
     <message>
         <location filename="src/callwidget.cpp" line="94"/>
         <source>&amp;Transfer to number</source>
-        <translation type="unfinished">Пе&amp;реключити на номер</translation>
+        <translation>Пе&amp;реключити на номер</translation>
     </message>
     <message>
         <location filename="src/callwidget.cpp" line="95"/>
         <source>Transfer the channel to the dialed number</source>
-        <translation type="unfinished">Переключити канал на набраний номер</translation>
+        <translation>Переключити канал на набраний номер</translation>
     </message>
     <message>
         <location filename="src/callwidget.cpp" line="99"/>
         <source>&amp;Park the call</source>
-        <translation type="unfinished">Поставити дзвінок на &amp;утримування</translation>
+        <translation>Поставити дзвінок на &amp;утримування</translation>
     </message>
     <message>
         <location filename="src/callwidget.cpp" line="100"/>
         <source>Park this call</source>
-        <translation type="unfinished">Поставити дзвінок на утримування</translation>
+        <translation>Поставити дзвінок на утримування</translation>
     </message>
     <message>
         <location filename="src/callwidget.cpp" line="149"/>
         <source>Unknown</source>
-        <translation type="unfinished">Невідомий</translation>
+        <translation>Невідомий</translation>
     </message>
     <message>
         <location filename="src/callwidget.cpp" line="151"/>
         <source>Conference room number %1</source>
-        <translation type="unfinished">Конференц-кімната номер %1</translation>
+        <translation>Конференц-кімната номер %1</translation>
     </message>
     <message>
         <location filename="src/callwidget.cpp" line="153"/>
         <source>Parked call in %1</source>
-        <translation type="unfinished">Дзвінок на утримуванні у %1</translation>
+        <translation>Дзвінок на утримуванні у %1</translation>
     </message>
     <message>
         <location filename="src/callwidget.cpp" line="155"/>
         <source>Parked call</source>
-        <translation type="unfinished">Дзвінок на утримуванні</translation>
+        <translation>Дзвінок на утримуванні</translation>
     </message>
     <message>
         <location filename="src/callwidget.cpp" line="157"/>
         <source>%1 : %2</source>
-        <translation type="unfinished">%1 : %2</translation>
+        <translation>%1 : %2</translation>
     </message>
 </context>
 <context>
@@ -62,12 +64,12 @@
     <message>
         <location filename="src/calls.cpp" line="48"/>
         <source>Calls</source>
-        <translation type="unfinished">Дзвінки</translation>
+        <translation>Дзвінки</translation>
     </message>
     <message>
         <location filename="src/calls.cpp" line="219"/>
         <source>Monitoring : %1</source>
-        <translation type="unfinished">Моніторинг : %1</translation>
+        <translation>Моніторинг : %1</translation>
     </message>
 </context>
 </TS>
--- plugins/conference/conference_uk.ts
+++ plugins/conference/conference_uk.ts
@@ -1,30 +1,32 @@
-<?xml version="1.0" encoding="utf8"?><!DOCTYPE TS><TS language="uk_UA" version="2.0">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk_UA">
 <context>
     <name>ConfChamber</name>
     <message>
         <location filename="src/confchamber.cpp" line="464"/>
         <source>&amp;Pause conference</source>
-        <translation type="unfinished">&amp;Призупинити конференцію</translation>
+        <translation>&amp;Призупинити конференцію</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="466"/>
         <source> Conference room </source>
-        <translation type="unfinished">Конференц-кімната</translation>
+        <translation>Конференц-кімната</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="513"/>
-        <source>This room is moderated. You can't see any participant, until an admin allow you in.</source>
-        <translation type="unfinished">Кімната модерується. Ви не можете переглядати відвідувачів, доки адміністратор не надасть вам таке право.</translation>
+        <source>This room is moderated. You can&apos;t see any participant, until an admin allow you in.</source>
+        <translation>Кімната модерується. Ви не можете переглядати відвідувачів, доки адміністратор не надасть вам таке право.</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="539"/>
         <source>&amp;Restart the conference</source>
-        <translation type="unfinished">&amp;Пере&amp;запустити конференцію</translation>
+        <translation>&amp;Пере&amp;запустити конференцію</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="541"/>
         <source>&amp;Pause the conference</source>
-        <translation type="unfinished">&amp;Призупинити конференцію</translation>
+        <translation>&amp;Призупинити конференцію</translation>
     </message>
 </context>
 <context>
@@ -32,110 +34,110 @@
     <message>
         <location filename="src/confchamber.cpp" line="54"/>
         <source>ID</source>
-        <translation type="unfinished">ID</translation>
+        <translation>ID</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="55"/>
         <source>Number</source>
-        <translation type="unfinished">Номер</translation>
+        <translation>Номер</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="56"/>
         <source>Name</source>
-        <translation type="unfinished">Ім'я</translation>
+        <translation>Ім&apos;я</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="57"/>
         <source>Since</source>
-        <translation type="unfinished">З</translation>
+        <translation>З</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="58"/>
         <source>Admin</source>
-        <translation type="unfinished">Адміністратор</translation>
+        <translation>Адміністратор</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="59"/>
         <source>K</source>
-        <translation type="unfinished">K</translation>
+        <translation>K</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="60"/>
         <source>R</source>
-        <translation type="unfinished">R</translation>
+        <translation>R</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="61"/>
         <source>A</source>
-        <translation type="unfinished">A</translation>
+        <translation>A</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="62"/>
         <source>T</source>
-        <translation type="unfinished">T</translation>
+        <translation>T</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="63"/>
         <source>M</source>
-        <translation type="unfinished">M</translation>
+        <translation>M</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="219"/>
         <source>Kick</source>
-        <translation type="unfinished">Від'єднати</translation>
+        <translation>Від&apos;єднати</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="222"/>
         <location filename="src/confchamber.cpp" line="227"/>
         <source>User already authed</source>
-        <translation type="unfinished">Користувача вже авторизовано</translation>
+        <translation>Користувача вже авторизовано</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="224"/>
         <source>Allow in</source>
-        <translation type="unfinished">Дати доступ</translation>
+        <translation>Дати доступ</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="229"/>
         <source>Talk to</source>
-        <translation type="unfinished">Поговорити з</translation>
+        <translation>Поговорити з</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="232"/>
         <source>User already recorded</source>
-        <translation type="unfinished">Користувача вже записано</translation>
+        <translation>Користувача вже записано</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="234"/>
         <source>Record conference until this user leaves</source>
-        <translation type="unfinished">Записувати конференцію, доки цей користувач не залишить кімнату</translation>
+        <translation>Записувати конференцію, доки цей користувач не залишить кімнату</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="239"/>
         <source>Unmute</source>
-        <translation type="unfinished">Ввімкнути звук</translation>
+        <translation>Ввімкнути звук</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="241"/>
         <source>Mute</source>
-        <translation type="unfinished">Вимкнути звук</translation>
+        <translation>Вимкнути звук</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="254"/>
         <location filename="src/confchamber.cpp" line="256"/>
         <source>Yes</source>
-        <translation type="unfinished">Так</translation>
+        <translation>Так</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="254"/>
         <location filename="src/confchamber.cpp" line="256"/>
         <source>No</source>
-        <translation type="unfinished">Ні</translation>
+        <translation>Ні</translation>
     </message>
     <message>
         <location filename="src/confchamber.cpp" line="262"/>
         <source>nobody</source>
-        <translation type="unfinished">ніхто</translation>
+        <translation>ніхто</translation>
     </message>
 </context>
 <context>
@@ -143,49 +145,49 @@
     <message>
         <location filename="src/conflist.cpp" line="50"/>
         <source>Room UID</source>
-        <translation type="unfinished">УІК кімнати</translation>
+        <translation>УІК кімнати</translation>
     </message>
     <message>
         <location filename="src/conflist.cpp" line="51"/>
         <source>Number</source>
-        <translation type="unfinished">Номер</translation>
+        <translation>Номер</translation>
     </message>
     <message>
         <location filename="src/conflist.cpp" line="52"/>
         <source>Name</source>
-        <translation type="unfinished">Ім'я</translation>
+        <translation>Ім&apos;я</translation>
     </message>
     <message>
         <location filename="src/conflist.cpp" line="53"/>
         <source>Pin code</source>
-        <translation type="unfinished">Пін код</translation>
+        <translation>Пін код</translation>
     </message>
     <message>
         <location filename="src/conflist.cpp" line="54"/>
         <source>Member count</source>
-        <translation type="unfinished">UNDEFINED</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/conflist.cpp" line="55"/>
         <source>Moderated</source>
-        <translation type="unfinished">Модерується</translation>
+        <translation>Модерується</translation>
     </message>
     <message>
         <location filename="src/conflist.cpp" line="56"/>
         <source>Started since</source>
-        <translation type="unfinished">Почалося</translation>
+        <translation>Почалося</translation>
     </message>
     <message>
         <location filename="src/conflist.cpp" line="117"/>
         <location filename="src/conflist.cpp" line="120"/>
         <source>No</source>
-        <translation type="unfinished">Ні</translation>
+        <translation>Ні</translation>
     </message>
     <message>
         <location filename="src/conflist.cpp" line="117"/>
         <location filename="src/conflist.cpp" line="120"/>
         <source>Yes</source>
-        <translation type="unfinished">Так</translation>
+        <translation>Так</translation>
     </message>
 </context>
 <context>
@@ -193,7 +195,7 @@
     <message>
         <location filename="src/conflist.cpp" line="236"/>
         <source>Get in room %1 (%2)</source>
-        <translation type="unfinished">Зайти у кімнату %1 (%2)</translation>
+        <translation>Зайти у кімнату %1 (%2)</translation>
     </message>
 </context>
 <context>
@@ -201,12 +203,12 @@
     <message>
         <location filename="src/conference.cpp" line="111"/>
         <source>Conference</source>
-        <translation type="unfinished">Конференція</translation>
+        <translation>Конференція</translation>
     </message>
     <message>
         <location filename="src/conference.cpp" line="117"/>
         <source>Conference room list</source>
-        <translation type="unfinished">Список конференц-кімнат</translation>
+        <translation>Список конференц-кімнат</translation>
     </message>
 </context>
 </TS>
