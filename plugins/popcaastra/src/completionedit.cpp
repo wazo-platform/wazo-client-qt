5c9bea25482d841abf1d2450e660b342317fc2f8
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2011-06-23 16:07:38 -0400

    POPC Aastra added a filtered lineedit/completer
    Added a widget to filter user input for searching thru conf, parking, contacts
    etc in the POPC xlet

---
+++ plugins/popcaastra/src/completionedit.cpp
@@ -0,0 +1,110 @@
+#include <QDebug>
+#include <QKeyEvent>
+#include <typeinfo>
+
+#include "completionedit.h"
+
+FilteredCompleter::FilteredCompleter(const QStringList & list, QObject * parent)
+    : QCompleter(parent), m_list(list), m_model()
+{
+    setModel(&m_model); // Default is QStringListModel
+}
+
+void FilteredCompleter::update(const QString & query)
+{
+    QStringList filtered = m_list.filter(query, caseSensitivity());
+    m_model.setStringList(filtered);
+    m_word = query;
+    complete(); // Start auto completion
+}
+
+const QString FilteredCompleter::word() const
+{
+    return m_word;
+}
+
+FilteredLineEdit::FilteredLineEdit(QWidget *parent)
+    : QLineEdit(parent), c(0)
+{
+}
+ 
+void FilteredLineEdit::setCompleter(QCompleter * completer)
+{
+    qDebug() << Q_FUNC_INFO;
+
+    if (c)
+        QObject::disconnect(c, 0, this, 0);
+ 
+    c = completer;
+    c->setCompletionMode(QCompleter::PopupCompletion);
+ 
+    if (!c)
+        return;
+ 
+    c->setWidget(this);
+    connect(completer, SIGNAL(activated(const QString &)),
+            this, SLOT(insertCompletion(const QString &)));
+}
+ 
+const QCompleter * FilteredLineEdit::completer() const
+{
+    qDebug() << Q_FUNC_INFO;
+
+    return c;
+}
+ 
+void FilteredLineEdit::insertCompletion(const QString & completion)
+{
+    qDebug() << Q_FUNC_INFO;
+
+    setText(completion);
+    selectAll();
+}
+ 
+ 
+void FilteredLineEdit::keyPressEvent(QKeyEvent * e)
+{
+    qDebug() << Q_FUNC_INFO;
+
+    if (c && c->popup() && c->popup()->isVisible())
+    {
+        // The following keys are forwarded by the completer to the widget
+        switch (e->key())
+        {
+        case Qt::Key_Enter:
+        case Qt::Key_Return:
+        case Qt::Key_Escape:
+        case Qt::Key_Tab:
+        case Qt::Key_Backtab:
+            e->ignore();
+            return; // Let the completer do default behavior
+        }
+    }
+    
+    bool isShortcut = (e->modifiers() & Qt::ControlModifier)
+            && e->key() == Qt::Key_E;
+    // Don't send the shortcut (CTRL-E) to the text edit
+    if (!isShortcut)
+        QLineEdit::keyPressEvent(e); 
+ 
+    if (!c)
+        return;
+ 
+    bool ctrlOrShift = e->modifiers() &
+            (Qt::ControlModifier | Qt::ShiftModifier);
+    if (!isShortcut && !ctrlOrShift && e->modifiers() != Qt::NoModifier)
+    {
+        c->popup()->hide();
+        return;
+    }
+ 
+    try {
+        (dynamic_cast<FilteredCompleter *>(c))->update(text());
+    } catch (const std::bad_cast & e) {
+        qDebug() << Q_FUNC_INFO << "Only FilteredCompleter are updated";
+    }
+    if (c && c->popup()) {
+        c->popup()->setCurrentIndex(c->completionModel()->index(0, 0));
+    }
+}
+
---
+++ plugins/popcaastra/src/completionedit.h
@@ -0,0 +1,66 @@
+#ifndef __COMPLETIONEDIT_H__
+#define __COMPLETIONEDIT_H__
+ 
+#include <QCompleter>
+#include <QLineEdit>
+#include <QString>
+#include <QStringList>
+#include <QStringListModel>
+ 
+/*! \brief A QCompleter with a filter option
+ *  
+ *  This completer add a filter method to remove entries from the completion
+ *  list to match the user input
+ */
+class FilteredCompleter : public QCompleter
+{
+    Q_OBJECT
+ 
+public:
+    /*! \brief Construct a FilterCompleter
+     *  \param list The list of words available for completion
+     *  \param parent The parent widget
+     */
+    FilteredCompleter(const QStringList & list, QObject * parent);
+    /*! \brief Update the content of the completer
+     *  \param query The term we are looking for
+     */
+    void update(const QString & word);
+    /*! \brief Get the typed word
+     *  \return the word that was typed by the user
+     */
+    const QString word() const;
+private:
+    QStringList m_list;         //!< Static content
+    QStringListModel m_model;   //!< Completion model
+    QString m_word;             //!< Current entry
+};
+ 
+/*! \brief A filtered line edit
+ *
+ *  A line edit with a content filter based on input
+ *  The content is matched at any position
+ *  ex: filtering "cal" will match "local"
+ */
+class FilteredLineEdit : public QLineEdit
+{
+    Q_OBJECT
+ 
+public:
+    FilteredLineEdit(QWidget *parent = 0);
+    /*! \brief Set a completer for the line edit
+     *  \param c a QCompleter
+     */
+    void setCompleter(QCompleter * c);
+    /*! \brief Retreves the QCompleter */
+    const QCompleter * completer() const;
+protected:
+    void keyPressEvent(QKeyEvent * e);
+private slots:
+    void insertCompletion(const QString & completion);
+private:
+    QCompleter * c;    //!< Completer
+};
+ 
+#endif // __COMPLETIONEDIT_H__
+
