3c4c01ed5c794863acb426e184f1c21b3b7e43ff
Sebastien Duthil <sduthil@avencall.com>
2012-09-27 13:19:31 -0400

    [refactor] add checks for null pointers in automatic test

--- xivoclient/src/remotecontrol_customer_info.cpp
+++ xivoclient/src/remotecontrol_customer_info.cpp
@@ -45,8 +45,13 @@ void RemoteControl::then_i_see_a_sheet_with_variables_and_values(const QStringLi
     }
 
     CustomerInfoPanel *xlet = static_cast<CustomerInfoPanel*>(m_exec_obj.win->m_xletlist.value("customerinfo"));
+    this->assert(xlet != NULL, "xlet null");
+
     QTabWidget *sheet_tab = xlet->m_tabs;
+    this->assert(sheet_tab != NULL, "sheet tab widget null");
+
     QWidget *current_tab = sheet_tab->currentWidget();
+    this->assert(current_tab != NULL, "current sheet tab null");
 
     int layout_header_offset = 3;
 
@@ -54,12 +59,23 @@ void RemoteControl::then_i_see_a_sheet_with_variables_and_values(const QStringLi
 
     foreach (const QVariant &variable, variables.toList()) {
         QVariantMap variable_map = variable.toMap();
-        const QString &variable_name = variable_map["Variable"].toString();
-        const QString &variable_value = variable_map["Value"].toString();
-        QLabel * variable_name_label = current_tab->findChild<QLabel*>(QString("label_name_%1").arg(widget_index));
-        QLabel * variable_value_label = current_tab->findChild<QLabel*>(QString("label_value_%1").arg(widget_index));
-        this->assert(variable_name_label->text() == variable_name);
-        this->assert(variable_value_label->text() == variable_value);
+
+        const QString &expected_variable_name = variable_map["Variable"].toString();
+        const QString &expected_variable_value = variable_map["Value"].toString();
+
+        const QString & name_of_name_label = QString("label_name_%1").arg(widget_index);
+        const QString & name_of_value_label = QString("label_value_%1").arg(widget_index);
+
+        QLabel * variable_name_label = current_tab->findChild<QLabel*>(name_of_name_label);
+        this->assert(variable_name_label != NULL, QString("can't find label %1").arg(name_of_name_label));
+        QLabel * variable_value_label = current_tab->findChild<QLabel*>(name_of_value_label);
+        this->assert(variable_value_label != NULL, QString("can't find label %1").arg(name_of_value_label));
+
+        const QString & variable_name = variable_name_label->text();
+        const QString & variable_value = variable_value_label->text();
+
+        this->assert(variable_name == expected_variable_name, QString("wrong name %1").arg(variable_name) );
+        this->assert(variable_value == expected_variable_value, QString("wrong value %1").arg(variable_value));
         ++widget_index;
     }
 }
