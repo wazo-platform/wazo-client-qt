1c718dcdaed49a8fafebabfb2f9646471eee47f0
Sebastien Duthil <sduthil@avencall.com>
2012-11-23 16:20:52 -0500

    fix unit-test launcher

--- unit-tests/alltests.sh
+++ unit-tests/alltests.sh
@@ -3,7 +3,7 @@
 # Launch all unit-tests
 
 testbins=$(find bin/tests -type f -print)
-for testbin in "$testbins" ; do
+for testbin in $testbins ; do
     "$testbin"
     "$testbin" -xunitxml -o unit-tests/unit-baselib.xml
 done
