22182d7c2526267d9707b2e56f7fa1e95d2c295f
Sebastien Duthil <sduthil@avencall.com>
2012-11-27 15:31:51 -0500

    add translation files for switchboard xlet

---
+++ xivoclient/i18n/switchboard_nl.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl_NL">
+<context>
+    <name>Switchboard</name>
+    <message>
+        <location filename="../src/xlets/switchboard/switchboard.cpp" line="52"/>
+        <source>Switchboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/xlets/switchboard/switchboard.cpp" line="149"/>
+        <source>%1 call(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SwitchboardPanel</name>
+    <message>
+        <location filename="../src/xlets/switchboard/switchboard.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/xlets/switchboard/switchboard.ui" line="42"/>
+        <source>&lt;b&gt;Incoming Call&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/xlets/switchboard/switchboard.ui" line="65"/>
+        <source>0 call</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- xivoclient/src/xlets/switchboard/switchboard.pro
+++ xivoclient/src/xlets/switchboard/switchboard.pro
@@ -3,3 +3,13 @@ HEADERS = *.h
 SOURCES = *.cpp
 FORMS = switchboard.ui
 TARGET = switchboardplugin
+
+TRANSLATIONS += $$ROOT_DIR/i18n/switchboard_en.ts
+TRANSLATIONS += $$ROOT_DIR/i18n/switchboard_es_ES.ts
+TRANSLATIONS += $$ROOT_DIR/i18n/switchboard_pt_BR.ts
+TRANSLATIONS += $$ROOT_DIR/i18n/switchboard_hu.ts
+TRANSLATIONS += $$ROOT_DIR/i18n/switchboard_de.ts
+TRANSLATIONS += $$ROOT_DIR/i18n/switchboard_ja.ts
+TRANSLATIONS += $$ROOT_DIR/i18n/switchboard_it.ts
+TRANSLATIONS += $$ROOT_DIR/i18n/switchboard_fr.ts
+TRANSLATIONS += $$ROOT_DIR/i18n/switchboard_nl.ts
