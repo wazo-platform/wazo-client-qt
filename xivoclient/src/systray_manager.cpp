32c904a05ca5c00c125b2213e96b372634517a42
Sebastien Duthil <sduthil@avencall.com>
2012-12-31 13:52:28 -0500

    start adding systray_manager with TDD

---
+++ xivoclient/src/systray_manager.cpp
@@ -0,0 +1,48 @@
+/* XiVO Client
+ * Copyright (C) 2007-2012, Avencall
+ *
+ * This file is part of XiVO Client.
+ *
+ * XiVO Client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with a Section 7 Additional
+ * Permission as follows:
+ *   This notice constitutes a grant of such permission as is necessary
+ *   to combine or link this software, or a modified version of it, with
+ *   the OpenSSL project's "OpenSSL" library, or a derivative work of it,
+ *   and to copy, modify, and distribute the resulting work. This is an
+ *   extension of the special permission given by Trolltech to link the
+ *   Qt code with the OpenSSL library (see
+ *   <http://doc.trolltech.com/4.4/gpl.html>). The OpenSSL library is
+ *   licensed under a dual license: the OpenSSL License and the original
+ *   SSLeay license.
+ *
+ * XiVO Client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XiVO Client.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QDebug>
+#include <QIcon>
+#include <QSystemTrayIcon>
+
+#include "systray_icon_manager.h"
+#include "systray_manager.h"
+
+SystrayManager::SystrayManager(const SystrayIconManager & systray_icon_manager,
+                               QSystemTrayIcon & qt_system_tray_icon)
+    : m_systray_icon_manager(systray_icon_manager),
+      m_qt_system_tray_icon(qt_system_tray_icon)
+{
+}
+
+void SystrayManager::change_icon(SystrayIcon new_icon_id)
+{
+    QIcon new_icon = this->m_systray_icon_manager.get_systray_icon(new_icon_id);
+    this->m_qt_system_tray_icon.setIcon(new_icon);
+}
---
+++ xivoclient/src/tests/test_systray_manager/test_systray_manager.pro
@@ -0,0 +1,13 @@
+include (../../../common-tests.pri)
+
+TARGET = test_systray_manager
+
+TEST_DIR = $${ROOT_DIR}/src/tests/test_systray_manager
+TESTED_DIR = $${ROOT_DIR}/src
+
+INCLUDEPATH += $${TESTED_DIR}
+INCLUDEPATH += $${MOC_DIR}
+
+SOURCES += $${TEST_DIR}/*.cpp
+HEADERS += $${TESTED_DIR}/systray_manager.h
+SOURCES += $${TESTED_DIR}/systray_manager.cpp
