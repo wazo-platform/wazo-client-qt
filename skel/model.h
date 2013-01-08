72fcadfcda9056e77aa5ed64c4d681ab62585eea
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2013-01-08 10:01:31 -0500

    Add a model class skeleton

---
+++ skel/model.h
@@ -0,0 +1,68 @@
+/* XiVO Client
+ * Copyright (C) 2007-2013, Avencall
+ *
+ * This file is part of XiVO Client.
+ *
+ * XiVO Client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with a Section 7 Additional
+ * Permission as follows:
+ *   This notice constitutes a grant of such permission as is necessary
+ *   to combine or link this software, or a modified version of it, with
+ *   the OpenSSL project's "OpenSSL" library, or a derivative work of it,
+ *   and to copy, modify, and distribute the resulting work. This is an
+ *   extension of the special permission given by Trolltech to link the
+ *   Qt code with the OpenSSL library (see
+ *   <http://doc.trolltech.com/4.4/gpl.html>). The OpenSSL library is
+ *   licensed under a dual license: the OpenSSL License and the original
+ *   SSLeay license.
+ *
+ * XiVO Client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XiVO Client.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __{CLASS_NAME}_MODEL_H__
+#define __{CLASS_NAME}_MODEL_H__
+
+#include <QAbstractTableModel>
+
+class MyModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+    public:
+        MyModel(QObject *parent = NULL);
+
+        int rowCount(const QModelIndex &) const;
+        int columnCount(const QModelIndex &) const;
+
+        bool removeRows(int, int, const QModelIndex &);
+
+        QVariant data(const QModelIndex&, int role = Qt::DisplayRole) const;
+        QVariant headerData(int,
+                            Qt::Orientation,
+                            int) const;
+
+    public slots:
+        void refreshColumn(int column_index);
+
+    public:
+        enum Columns {
+            NB_COL
+        };
+
+    private:
+        QVariant dataDisplay(int row, int column) const;
+        QVariant dataBackground(int row, int column) const;
+        QVariant dataTooltip(int row, int column) const;
+
+        QString m_headers[NB_COL];
+};
+
+#endif
