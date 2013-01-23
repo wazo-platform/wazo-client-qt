da9bef1a428b6557ec60ea8cee599b249e169fae
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2013-01-23 09:57:04 -0500

    Add a virtual destructor to PhoneInfo

--- baselib/src/storage/phoneinfo.h
+++ baselib/src/storage/phoneinfo.h
@@ -42,6 +42,7 @@ class BASELIB_EXPORT PhoneInfo : public XInfo
 {
     public:
         PhoneInfo(const QString &, const QString &);
+        ~PhoneInfo() {}
         bool updateConfig(const QVariantMap &);
         bool updateStatus(const QVariantMap &);
         const QString & context() const { return m_context; };
