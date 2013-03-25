08616997b763a220955575ad193cd1be89cca67b
Sebastien Duthil <sduthil@avencall.com>
2013-03-25 10:08:00 -0400

    implement AgentStatusDelegate

--- xivoclient/src/xlets/agent_status_dashboard/agent_status_delegate.cpp
+++ xivoclient/src/xlets/agent_status_dashboard/agent_status_delegate.cpp
@@ -10,11 +10,15 @@ AgentStatusDelegate::~AgentStatusDelegate()
 {
 }
 
-void AgentStatusDelegate::paint(QPainter * painter, const QStyleOptionViewItem & style, const QModelIndex & index) const
+void AgentStatusDelegate::paint(QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index) const
 {
+    QWidget & widget = m_widget_storage.getWidget(index);
+    QPoint offset = option.rect.topLeft();
+    widget.render(painter, offset);
 }
 
 QSize AgentStatusDelegate::sizeHint(const QStyleOptionViewItem & style, const QModelIndex & index) const
 {
-    return QSize();
+    QWidget & widget = m_widget_storage.getWidget(index);
+    return widget.size();
 }
