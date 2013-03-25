5fca22ad68b52a970f8d49eb7b21dbd99c8326ed
Sebastien Duthil <sduthil@avencall.com>
2013-03-25 11:49:31 -0400

    fill firstname lastname and color in AgentStatus widgets

--- xivoclient/src/xlets/agent_status_dashboard/agent_status_delegate.cpp
+++ xivoclient/src/xlets/agent_status_dashboard/agent_status_delegate.cpp
@@ -1,4 +1,7 @@
 #include <QPainter>
+#include <QLabel>
+
+#include <xletlib/agents_model.h>
 
 #include "agent_status_widget_storage.h"
 #include "agent_status_delegate.h"
@@ -14,7 +17,19 @@ AgentStatusDelegate::~AgentStatusDelegate()
 
 void AgentStatusDelegate::paint(QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index) const
 {
+    const QAbstractItemModel * model = index.model();
+    QModelIndex agent_firstname_index = model->index(index.row(), AgentsModel::FIRSTNAME);
+    QString agent_firstname = model->data(agent_firstname_index).toString();
+    QModelIndex agent_lastname_index = model->index(index.row(), AgentsModel::LASTNAME);
+    QString agent_lastname = model->data(agent_lastname_index).toString();
+    QModelIndex agent_availability_index = model->index(index.row(), AgentsModel::AVAILABILITY);
+    QColor agent_availability_color = model->data(agent_availability_index, Qt::BackgroundRole).value<QColor>();
+
     QWidget & widget = m_widget_storage.getWidget(index);
+    QLabel * agent_name_label = widget.findChild<QLabel *>("agent_name_label");
+    agent_name_label->setText(QString("%1 %2").arg(agent_firstname, agent_lastname));
+    widget.setStyleSheet(QString("#AgentStatus {border:10px solid %1;}").arg(agent_availability_color.name()));
+
     QPoint offset = option.rect.topLeft();
     widget.render(painter, painter->deviceTransform().map(offset));
 }
