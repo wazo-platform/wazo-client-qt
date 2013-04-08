c004fb8de209b5ca8ac689dec92185fc89098ebc
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2013-04-08 16:25:03 -0400

    Reset original red color in agent list xlet

--- xivoclient/src/xletlib/agents_model.cpp
+++ xivoclient/src/xletlib/agents_model.cpp
@@ -339,7 +339,7 @@ QVariant AgentsModel::dataBackgroundAvailability(const AgentInfo * agent) const
     case AgentInfo::AVAILABLE:
         return Qt::green;
     case AgentInfo::UNAVAILABLE:
-        return QColor("#ed8114");
+        return Qt::red;
     default:
         return QVariant();
     }
