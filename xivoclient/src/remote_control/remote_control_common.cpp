da4ddedd50415cf3f8590c02a439b74177de3068
Cedric Abunar <cabunar@avencall.com>
2013-05-02 08:36:16 -0400

    improve variable name

--- xivoclient/src/remote_control/remote_control_common.cpp
+++ xivoclient/src/remote_control/remote_control_common.cpp
@@ -119,10 +119,10 @@ void RemoteControl::configure(const QVariantList &list)
         display_profile_box->setChecked(display_profile);
     }
 
-    if (args.find("customerinfo") != args.end()) {
-        bool customerinfo = args["customerinfo"].toBool();
-        QCheckBox *customer_info_box = m_exec_obj.win->m_configwindow->findChild<QCheckBox*>(QString("enable_customer_info"));
-        customer_info_box->setChecked(customerinfo);
+    if (args.find("enable_screen_popup") != args.end()) {
+        bool enable_screen_popup = args["enable_screen_popup"].toBool();
+        QCheckBox *enable_screen_popup = m_exec_obj.win->m_configwindow->findChild<QCheckBox*>(QString("enable_customer_info"));
+        customer_info_box->setChecked(enable_screen_popup);
     }
 
     if (args.find("hide_unlogged_agents_for_xlet_queue_members") != args.end()) {
