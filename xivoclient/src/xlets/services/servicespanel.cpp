68688b9d7122df39f39241052743a248448920b6
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2014-02-17 16:01:46 -0500

    Move the services xlet to the new plugin system

--- xivoclient/src/xlets/services/servicespanel.cpp
+++ xivoclient/src/xlets/services/servicespanel.cpp
@@ -43,8 +43,6 @@
 const QStringList chkcapas = (QStringList() << "enablevoicemail" << "incallfilter" << "enablednd");
 const QStringList fwdcapas = (QStringList() << "fwdunc" << "fwdrna" << "fwdbusy");
 
-Q_EXPORT_PLUGIN2(xletservicesplugin, XLetServicesPlugin);
-
 XLet* XLetServicesPlugin::newXLetInstance(QWidget *parent)
 {
     b_engine->registerTranslation(":/obj/services_%1");
--- xivoclient/src/xlets/services/servicespanel.h
+++ xivoclient/src/xlets/services/servicespanel.h
@@ -89,6 +89,7 @@ class XLetServicesPlugin : public QObject, XLetInterface
 {
     Q_OBJECT
     Q_INTERFACES(XLetInterface)
+    Q_PLUGIN_METADATA(IID "com.avencall.Plugin.XLetInterface/1.2" FILE "xletservices.json")
 
     public:
         XLet* newXLetInstance(QWidget *parent=0);
