3e7dce923c46dab553ed8ee3fc89b527651a871a
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2014-02-18 08:07:40 -0500

    Move the agents xlet to the new plugin system

--- xivoclient/src/xlets/agents/agents.h
+++ xivoclient/src/xlets/agents/agents.h
@@ -49,11 +49,10 @@ class XLetAgentsPlugin : public QObject, XLetInterface
 {
     Q_OBJECT
     Q_INTERFACES(XLetInterface)
+    Q_PLUGIN_METADATA(IID "com.avencall.Plugin.XLetInterface/1.2" FILE "xletagents.json")
 
     public:
         XLet *newXLetInstance(QWidget *parent=0);
 };
-
-Q_EXPORT_PLUGIN2(xletagentsplugin, XLetAgentsPlugin);
 
 #endif /* __AGENTSPANEL_H__ */
