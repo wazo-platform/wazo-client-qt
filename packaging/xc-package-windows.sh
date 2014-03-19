6cf303a014c5e17e6190d521caa3a07a9d9c60ec
Sebastien Duthil <sduthil@avencall.com>
2014-03-19 11:53:13 -0400

    update lib names for windows packaging

--- packaging/xc-package-windows.sh
+++ packaging/xc-package-windows.sh
@@ -34,21 +34,24 @@ rm -rf $DEST_DIR/qtlibs
 mkdir $DEST_DIR/qtlibs
 cp $MINGW_DIR/bin/\
 {libstdc++-6.dll,\
-libgcc_s_dw2-1.dll,\
-mingwm10.dll} $DEST_DIR/qtlibs
+libgcc_s_dw2-1.dll} $DEST_DIR/qtlibs
 cp $QT_DIR/bin/\
-{QtCore4.dll,\
-QtGui4.dll,\
-QtMultimedia4.dll,\
-QtNetwork4.dll,\
-QtSql4.dll,\
-QtXml4.dll,\
-QtXmlPatterns4.dll,\
-libeay32.dll,\
-ssleay32.dll} $DEST_DIR/qtlibs
-rm -rf $DEST_DIR/qtlibs/{imageformats,sqldrivers}
-mkdir $DEST_DIR/qtlibs/{imageformats,sqldrivers}
+{Qt5Core.dll,\
+Qt5Gui.dll,\
+Qt5Multimedia.dll,\
+Qt5Network.dll,\
+Qt5Sql.dll,\
+Qt5Widgets.dll,\
+Qt5Xml.dll,\
+Qt5XmlPatterns.dll,\
+libwinpthread-1.dll,\
+icuin51.dll,\
+icuuc51.dll,\
+icudt51.dll} $DEST_DIR/qtlibs
+rm -rf $DEST_DIR/qtlibs/{imageformats,platforms,sqldrivers}
+mkdir $DEST_DIR/qtlibs/{imageformats,platforms,sqldrivers}
 cp -r $QT_DIR/plugins/imageformats/*.dll $DEST_DIR/qtlibs/imageformats
+cp -r $QT_DIR/plugins/platforms/*.dll $DEST_DIR/qtlibs/platforms
 cp -r $QT_DIR/plugins/sqldrivers/*.dll $DEST_DIR/qtlibs/sqldrivers
 
 # Qt translations
